99a3418476767eb9699e2ccf6187c9be48c38e5c
rimonnnn <rimonseha123@gmail.com>
2025-10-03 19:42:22 +0300

    - delete unnesessary files

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test_application_1/features/profile/pages/rimon.dart';
 import 'package:flutter_test_application_1/features/profile/pages/welcome.dart';
 import 'package:flutter_test_application_1/features/profile/widgets/login.dart';
-import 'package:flutter_test_application_1/features/profile/widgets/rimon_info.dart';
 import 'package:flutter_test_application_1/features/profile/widgets/sign_up.dart';
-import 'package:flutter_test_application_1/features/profile/widgets/social_media_row.dart';
 import 'features/profile/pages/profile_page.dart';
 
 void main() {
@@ -23,11 +21,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/welcome': (context) => const Welcome(),
         '/profile': (context) => const ProfilePage(),
-        '/social': (context) => const SocialMediaRow(),
+
         '/rimon': (context) => const Rimon(),
         '/login': (context) => const Login(),
         '/sign_up': (context) => const SignUp(),
-        '/rimon_info': (context) => const RimonInfo(),
       },
     );
   }
